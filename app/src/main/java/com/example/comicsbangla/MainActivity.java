106ab54dd9aae9bfe37e2d4a85b99027cc3d894f
SOUMIK <sakibsoumik01@gmail.com>
2020-04-07 20:00:48 +0600

    update and retrieve list of keep reading tab from a file

--- app/src/main/java/com/example/comicsbangla/MainActivity.java
+++ app/src/main/java/com/example/comicsbangla/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.comicsbangla;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -13,11 +18,20 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE =1 ;
     ViewFlipper v_flipper;  //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE);
+
+        }
         setContentView(R.layout.activity_main);
 
         //Initialize and Assign Variable for Bottom navbar
