fa0e9aafbcc741bd5b5453c6774e9f738b89a5bb
fairuznawar122 <fairuznawarupoma@gmail.com>
2020-04-17 19:17:12 +0600

    Upload Page Updated

--- app/src/main/java/com/example/comicsbangla/Upload.java
+++ app/src/main/java/com/example/comicsbangla/Upload.java
@@ -5,32 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Upload extends AppCompatActivity {
     FirebaseAuth mAuth;
+    EditText userComicName,userComicAuthor,userComicDes;
     CheckBox action,adventure,horror,contemp,fiction,comedy;
     ImageButton uploadFile;
     Button uploadSubmit;
     String cat;
+    String userComicNameString,userComicDesString,userComicAuthorString;
+    TextView error;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
         mAuth=FirebaseAuth.getInstance();
 
+        userComicName=findViewById(R.id.userComicName);
+        userComicDes=findViewById(R.id.userComicDes);
+        userComicAuthor=findViewById(R.id.userComicAuthorName);
+
+        error=findViewById(R.id.errorText);
         contemp= findViewById(R.id.checkBoxContemporary);
         action= findViewById(R.id.checkBoxAction);
         adventure= findViewById(R.id.checkBoxAdventure);
@@ -40,45 +54,135 @@ public class Upload extends AppCompatActivity {
         uploadSubmit= findViewById(R.id.uploadButton);
         uploadFile= findViewById(R.id.upload_file);
 
+        //------------------Error Handling For Check Box ------------------------\\
+        contemp.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                error.setText(null);
+            }
+        });
+        comedy.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                error.setText(null);
+            }
+        });
+        horror.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                error.setText(null);
+            }
+        });
+        action.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                error.setText(null);
+            }
+        });
+        adventure.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                error.setText(null);
+            }
+        });
+        fiction.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                error.setText(null);
+            }
+        });
+        //------------------Error Handling For Check Box ------------------------\\
+
+
+        //-------------- Upload Button is Clicked : Checking Inputs---------------\\
         uploadSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final StringBuilder stringBuilder=new StringBuilder();
-
+                int i=0;
+                boolean validInput=true;
+                // Checking which boxes were checked
                 if(contemp.isChecked()){
                     String value= contemp.getText().toString();
                     stringBuilder.append(value+",");
+                    i++;
+                    error.setText(null);
                 }
+                else error.setText(null);
                 if(action.isChecked()){
                     String value= action.getText().toString();
                     stringBuilder.append(value+",");
+                    i++;
+                    error.setText(null);
                 }
+                else error.setText(null);
                 if(adventure.isChecked()){
                     String value= adventure.getText().toString();
                     stringBuilder.append(value+",");
+                    i++;
+                    error.setText(null);
                 }
+                else error.setText(null);
                 if(horror.isChecked()){
                     String value= horror.getText().toString();
                     stringBuilder.append(value+",");
+                    i++;
+                    error.setText(null);
                 }
+                else error.setText(null);
                 if(comedy.isChecked()){
                     String value= comedy.getText().toString();
                     stringBuilder.append(value+",");
+                    i++;
+                    error.setText(null);
                 }
+                else error.setText(null);
                 if(fiction.isChecked()){
                     String value= fiction.getText().toString();
                     stringBuilder.append(value+",");
+                    i++;
+                    error.setText(null);
                 }
-                cat=stringBuilder.toString();
-                Toast.makeText(Upload.this, "Catagories : "+ cat, Toast.LENGTH_LONG).show();
+                else error.setText(null);
+                if(i>3){
+                    validInput=false;
+                    error.setText("৩টির বেশি ক্যাটেগরি গ্রহণযোগ্য নয়!");
+                    error.setError(null);
+                }
+                else{
+                    cat=stringBuilder.toString();
+                    validInput=true;
+                }
+         //User Inputs in String
+                userComicAuthorString= userComicAuthor.getText().toString();
+                userComicDesString= userComicDes.getText().toString();
+                userComicNameString=userComicName.getText().toString();
+                if(TextUtils.isEmpty(userComicNameString)){
+                    userComicName.setError("কমিক্সের নাম পূরণ করুন!");
+                    validInput=false;
+                }
+                if(TextUtils.isEmpty(userComicAuthorString)){
+                    userComicAuthor.setError("লেখকের নাম পূরণ করুন!");
+                    validInput=false;
+                }
+                if(TextUtils.isEmpty(userComicDesString)){
+                    userComicDes.setError("কমিক্সের সংক্ষিপ্ত বর্ণনা দিন!");
+                    validInput=false;
+                }
+     //============ IDEAL CASE ================\\
+                if(validInput){
+                    Toast.makeText(Upload.this, userComicNameString+"\n"+userComicAuthorString+"\n"+userComicDesString+"\n"+ cat, Toast.LENGTH_LONG).show();
+                }
+
             }
 
         });
 
+        //----------Upload File Arrow Icon Is Clicked -------\\
         uploadFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Upload.this, "Dushtu", Toast.LENGTH_SHORT).show();
+                Toast.makeText(Upload.this, "Dushtu!", Toast.LENGTH_SHORT).show();
             }
 
         });
