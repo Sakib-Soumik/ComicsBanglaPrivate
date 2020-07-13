426cb9a4a9ffd243ff2182cb107027c2736131fa
fairuznawar122 <fairuznawarupoma@gmail.com>
2020-07-14 01:14:44 +0600

    Search Page Layout

--- app/src/main/java/com/example/comicsbangla/search_page.java
+++ app/src/main/java/com/example/comicsbangla/search_page.java
@@ -2,13 +2,33 @@ package com.example.comicsbangla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class search_page extends AppCompatActivity {
 
+    ImageButton searchClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+        searchClick=findViewById(R.id.searchClicked);
+
+        searchClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(search_page.this, "clicked", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
+
+
+
     }
+
 }
