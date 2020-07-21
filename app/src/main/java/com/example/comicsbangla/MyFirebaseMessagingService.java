842eaa289ba44a96fde67e1ed2a9b1a2f5a99a0d
SOUMIK <sakibsoumik01@gmail.com>
2020-07-21 07:35:24 +0600

    implemented notification service

--- app/src/main/java/com/example/comicsbangla/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/comicsbangla/MyFirebaseMessagingService.java
@@ -114,7 +114,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param token The new token.
      */
     private void sendRegistrationToServer(String token) {
-        // TODO: Implement this method to send token to your app server.
+        
     }
 
     /**
